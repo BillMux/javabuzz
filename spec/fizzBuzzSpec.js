af3025e7d3e6d73ab044f558c457b44597508610
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-11-01 19:02:00 +0000

    refactor

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -22,20 +22,20 @@ describe('fizzBuzz', function() {
     it('not divisible by 15', function() {
         expect(fizzbuzz._isDivisibleBy(4, 15)).toBe(false);
     });
+  });
 
-    describe('when played, says', function() {
-      it('"Fizz" when a number is divisible by 3', function() {
-        expect(fizzbuzz.says(3)).toEqual('Fizz');
-      });
-      it('"Buzz" when a number is divisible by 5', function() {
-        expect(fizzbuzz.says(5)).toEqual('Buzz');
-      });
-      it('"FizzBuzz" when a number is divisible by 15', function() {
-        expect(fizzbuzz.says(15)).toEqual('FizzBuzz');
-      });
-      it('the number when a number is not divisible by 3 or 5', function() {
-        expect(fizzbuzz.says(4)).toEqual(4);
-      });
+  describe('when played, says', function() {
+    it('"Fizz" when a number is divisible by 3', function() {
+      expect(fizzbuzz.says(3)).toEqual('Fizz');
+    });
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(fizzbuzz.says(5)).toEqual('Buzz');
+    });
+    it('"FizzBuzz" when a number is divisible by 15', function() {
+      expect(fizzbuzz.says(15)).toEqual('FizzBuzz');
+    });
+    it('the number when a number is not divisible by 3 or 5', function() {
+      expect(fizzbuzz.says(4)).toEqual(4);
     });
   });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,23 +1,17 @@
 var fizzBuzz = function(){};
-
-  fizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
-    return (number % divisor === 0);
-  };
-  fizzBuzz.prototype.says = function(number) {
-
-    if (this._isDivisibleBy(number, 15)) {
-      return 'FizzBuzz';
-    }
-
-    if (this._isDivisibleBy(number, 3)) {
-      return 'Fizz';
-    }
-
-    if (this._isDivisibleBy(number, 5)) {
-      return 'Buzz';
-    }
-    else {
-      return number;
-    }
-
-  };
+fizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
+fizzBuzz.prototype.says = function(number) {
+  string = ''
+  if (this._isDivisibleBy(number, 3)) {
+    string += 'Fizz';
+  }
+  if (this._isDivisibleBy(number, 5)) {
+    string += 'Buzz';
+  }
+  if (!this._isDivisibleBy(number, 3) && !this._isDivisibleBy(number, 5)) {
+    string = number;
+  }
+  return string
+};
